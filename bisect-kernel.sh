aae2a8dcb058019da5f0bdf005057eaf5200a66e
Max Görner <5477952+MaxG87@users.noreply.github.com>
2022-01-30 18:22:46 +0100

    Make bisect-script more robust

--- bisect-kernel.sh
+++ bisect-kernel.sh
@@ -3,8 +3,21 @@
 set -euo pipefail
 
 KERNEL_DIR=~/Werkbank/Hobbys/Linux-Kernel/linux/
+KCONFIG="$KERNEL_DIR/.config"
 WIFI_DIR=~/Werkbank/Hobbys/rtl88x2bu
-rm -f "$KERNEL_DIR/.config"
-yes "" | make -C "$KERNEL_DIR" localmodconfig > /dev/null || true
+
+rm -f "$KCONFIG"
+cp "$(ls -1 /boot/config-5.10.0-* | sort -n | tail -n1)" "$KCONFIG"
+"$KERNEL_DIR/scripts/config" --file "$KCONFIG" \
+    --disable SYSTEM_TRUSTED_KEYS \
+    --enable OF_OVERLAY
+yes "" | make -C "$KERNEL_DIR" localmodconfig || true
+# "$KERNEL_DIR/scripts/config" --file "$KERNEL_DIR/.config" \
+#     --enable CONFIG_MODULES \
+#     --enable CONFIG_WLAN \
+#     --enable CONFIG_WIRELESS \
+#     --enable CONFIG_CFG80211 \
+#     --enable CONFIG_USB
+
 make -C "$KERNEL_DIR" -j4 dir-pkg
 make -C "$WIFI_DIR" KBASE=/home/mgoerner/Werkbank/Hobbys/Linux-Kernel/linux -j4
