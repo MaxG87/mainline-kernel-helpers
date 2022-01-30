06c3d1574745431195871438c46428ac21841ae9
Max Görner <5477952+MaxG87@users.noreply.github.com>
2022-01-30 18:22:46 +0100

    Replace egrep by 'grep -E'

--- get-mainline.sh
+++ get-mainline.sh
@@ -6,12 +6,12 @@ KVER="$1"
 BASE_URL="https://kernel.ubuntu.com/~kernel-ppa/mainline/v${KVER}"
 
 curl -s "${BASE_URL}/" |
-egrep -A 7 'Build for amd64' |
-egrep -o 'linux[^"]+deb' |
+grep -A 7 'Build for amd64' |
+grep -Eo 'linux[^"]+deb' |
 sort -u |
-egrep -v lowlatency |
-while read deb
+grep -v lowlatency |
+while read -r deb
 do
   echo About to download "$deb".
-  curl -s "$BASE_URL/$deb" > $deb
+  curl -s "$BASE_URL/$deb" > "$deb"
 done
