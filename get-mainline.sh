c2dabffffac7475a6401563ca9662122fc1439a2
Max Görner <5477952+MaxG87@users.noreply.github.com>
2022-02-05 18:01:29 +0100

    Adapt get-mainline to structure of v5.16.6

--- get-mainline.sh
+++ get-mainline.sh
@@ -9,11 +9,12 @@ begin='(Build for amd64|Test amd64\/build) succeeded'
 end='(Build for|Test) .* succeeded'
 
 curl -s "${BASE_URL}/" |
-sed -nE "/${begin}/,/${end}/ {s/.*(linux[^\"]+\.deb).*/\1/p}" |
+sed -nE "/${begin}/,/${end}/ {s/.*<a href\=\"(.+\.deb)\">.*/\1/p}" |
 sort -u |
 grep -v lowlatency |
 while read -r deb
 do
-  echo About to download "$deb".
-  curl -s "$BASE_URL/$deb" > "$deb"
+    local_file=${deb#*/}
+    echo About to download "$deb".
+    curl -s "$BASE_URL/$deb" > "$local_file"
 done
