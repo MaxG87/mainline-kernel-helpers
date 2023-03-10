f770f7d9bd259d44fa00ef3cc02d8c6c76c87638
Max Görner <5477952+MaxG87@users.noreply.github.com>
2023-03-10 12:30:34 +0100

    Baue Kernel mit bindep-pkg
    Es scheint, als reichen mit bindeb-pkg gebaute Kernel für die
    alltäglichen Belange aus. Allerdings beginnt die Kompilation nun nicht
    mit einem `make clean`, sodass aufeinanderfolgende Bauprozesse nicht von
    Grund auf neu beginnen müssen.
    
    Um diesen Effekt nicht kaputt zu machen, wurde die distclean-Anweisung
    entfernt.

--- build-kernel.sh
+++ build-kernel.sh
@@ -8,14 +8,13 @@ then
     exit 1
 fi
 
-# Must remove all linux-* files. The target deb-pkg creates non-deb-files too.
+# Must remove all linux-* files. The target bindeb-pkg creates non-deb-files too.
 rm -f ../linux-*.{buildinfo,changes,deb,dsc,diff.gz,tar.gz}
 rm -f .config
 rm -rf "../linux.orig"
 
-make distclean
 cp "$(find /boot -maxdepth 1 -type f -name 'config-[0-9].[0-9].[0-9]-[0-9]-amd64' | tail -n1)" .config
 scripts/config --disable DEBUG_INFO
 scripts/config --disable SYSTEM_TRUSTED_KEYS
 yes "" | make oldconfig
-KCFLAGS="-march=native -O2 -pipe" KCPPFLAGS="-march=native -O2 -pipe" make -j "$(nproc)" deb-pkg
+KCFLAGS="-march=native -O2 -pipe" KCPPFLAGS="-march=native -O2 -pipe" make -j "$(nproc)" bindeb-pkg
