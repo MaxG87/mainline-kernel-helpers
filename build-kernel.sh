3bed60f949b73a067e323b59e9ce4cb250cb2df8
Max Görner <5477952+MaxG87@users.noreply.github.com>
2023-04-25 10:06:50 +0200

    Deaktiviere Erzeugung von Debug-Informationen
    Debug-Informationen sind hier nicht zu gebrauchen, da es an Wissen
    fehlt, wie von diesen zu profitieren ist. Gleichzeitig verlängern sie
    aber den Kompiliervorgang erheblich.
    
    Der vorhergehende Versuch, Debug-Informationen abzustellen, lief ins
    Leere, da er beim Start der Kompilierung direkt rückgängig gemacht wurde
    [0]. Es müssen dafür deutlich mehr Optionen angepasst werden [1]. Dies
    wird mit diesem Commit getan.
    
    [0]: https://superuser.com/questions/1780125/linux-kernel-compilation-debug-info-generated-albeit-disabling-debug-info
    [1]: https://superuser.com/questions/925079/compile-linux-kernel-deb-pkg-target-without-generating-dbg-package

--- build-kernel.sh
+++ build-kernel.sh
@@ -2,6 +2,17 @@
 
 set -eu
 
+function deactivate-debug-info() {
+    scripts/config --undefine DEBUG_INFO
+    scripts/config --undefine DEBUG_INFO_COMPRESSED
+    scripts/config --undefine DEBUG_INFO_REDUCED
+    scripts/config --undefine DEBUG_INFO_SPLIT
+    scripts/config --undefine GDB_SCRIPTS
+    scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
+    scripts/config --set-val  DEBUG_INFO_DWARF5 n
+    scripts/config --set-val  DEBUG_INFO_NONE y
+}
+
 if [[ ! -f README || "$(head -1 README)" != "Linux kernel" ]]
 then
     echo "Falsches Verzeichnis" >&2
@@ -14,7 +25,8 @@ rm -f .config
 rm -rf "../linux.orig"
 
 cp "$(find /boot -maxdepth 1 -type f -name 'config-[0-9].[0-9].[0-9]-[0-9]-amd64' | tail -n1)" .config
-scripts/config --disable DEBUG_INFO
+
+deactivate-debug-info
 yes "" | make -j "$(nproc)" oldconfig
 
 KCFLAGS="-march=native -O2 -pipe" KCPPFLAGS="-march=native -O2 -pipe" make -j "$(nproc)" bindeb-pkg
