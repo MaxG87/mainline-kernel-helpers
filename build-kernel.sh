8f45b1f70f59492d03627b727bff7622511b9db4
Max Görner <5477952+MaxG87@users.noreply.github.com>
2023-05-29 11:29:20 +0200

    Erstelle CLI-Parser und Hilfsfunktion

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 set -eu
+BUILDTARGET_DEFAULT=bindeb-pkg
+CONFIGTARGET_DEFAULT=oldconfig
+
+function print_usage() {
+    cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [--config-target] [--build-target]
+
+Configure and build Linux-Kernel
+
+Note that values for --build-target and --config-target will be passed through
+to \`make\` without further checks. If used maliciously, a whole compilation
+could be started in the configuration phase already.
+
+Available options:
+
+-h, --help       Print this help and exit
+-v, --verbose    Print script debug info
+--build-target   Compilation target. Will be passed through to \`make\`. Defaults to "$BUILDTARGET_DEFAULT".
+--config-target  Configuration target. Will be passed through to \`make\`. Defaults to "$CONFIGTARGET_DEFAULT".
+EOF
+    exit
+}
+
 
 function deactivate-debug-info() {
     scripts/config --undefine DEBUG_INFO
@@ -27,6 +50,29 @@ function configure-kernel() {
     deactivate-signing
     yes "" | make -j "$(nproc)" oldconfig
 }
+
+BUILDTARGET="$BUILDTARGET_DEFAULT"
+CONFIGTARGET="$CONFIGTARGET_DEFAULT"
+MAKE_VERBOSITY="-s"
+while [[ $# -gt 0 ]]
+do
+    case "${1-}" in
+        -h | --help) print_usage ;;
+        -v | --verbose) MAKE_VERBOSITY= ;;
+        --build-target)
+            BUILDTARGET="${2-}"
+            shift
+            ;;
+        --config-target)
+            CONFIGTARGET="${2-}"
+            shift
+            ;;
+        *) die "Unknown option: $1" ;;
+    esac
+    shift
+done
+
+
 
 if [[ ! -f README || "$(head -1 README)" != "Linux kernel" ]]
 then
