199528e1fa631239da33c1776c84523fe0d8a276
Max Görner <5477952+MaxG87@users.noreply.github.com>
2024-01-16 10:53:41 +0100

    Wende shellfmt-Formatierungen an

--- bisect-kernel.sh
+++ bisect-kernel.sh
@@ -5,7 +5,7 @@ set -eu
 CLEANUP_DEFAULT="true"
 
 function print_usage() {
-    cat <<EOF
+	cat <<EOF
 Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
 
 Build Linux-Kernel and RTL88x2BU in conjunction.
@@ -19,47 +19,45 @@ Available options:
 --kernel-dir    Directory of the Linux Kernel source code
 --wifi-dir      Directory of the RTL88x2BU source code
 EOF
-    exit
+	exit
 }
 
 msg() {
-    echo >&2 -e "${1-}"
+	echo >&2 -e "${1-}"
 }
 
 die() {
-    local msg=$1
-    local code=${2-1} # default exit status 1
-    msg "$msg"
-    exit "$code"
+	local msg=$1
+	local code=${2-1} # default exit status 1
+	msg "$msg"
+	exit "$code"
 }
 
 MAKE_VERBOSITY="-s"
 CLEANUP=$CLEANUP_DEFAULT
 FULL_BUILD="false"
-while [[ $# -gt 0 ]]
-do
-    case "${1-}" in
-        -h | --help) print_usage ;;
-        -v | --verbose) MAKE_VERBOSITY= ;;
-        --cleanup) CLEANUP="true" ;;
-        --no-cleanup) CLEANUP="false" ;;
-        --kernel-dir)
-            KERNEL_DIR="${2-}"
-            shift
-            ;;
-        --full-build) FULL_BUILD="true" ;;
-        --wifi-dir)
-            WIFI_DIR="${2-}"
-            shift
-            ;;
-        *) die "Unknown option: $1" ;;
-    esac
-    shift
+while [[ $# -gt 0 ]]; do
+	case "${1-}" in
+	-h | --help) print_usage ;;
+	-v | --verbose) MAKE_VERBOSITY= ;;
+	--cleanup) CLEANUP="true" ;;
+	--no-cleanup) CLEANUP="false" ;;
+	--kernel-dir)
+		KERNEL_DIR="${2-}"
+		shift
+		;;
+	--full-build) FULL_BUILD="true" ;;
+	--wifi-dir)
+		WIFI_DIR="${2-}"
+		shift
+		;;
+	*) die "Unknown option: $1" ;;
+	esac
+	shift
 done
 
-if ! [[ ${KERNEL_DIR:+1} && ${WIFI_DIR:+1} ]]
-then
-    die "Both KERNEL_DIR and WIFI_DIR must be set!"
+if ! [[ ${KERNEL_DIR:+1} && ${WIFI_DIR:+1} ]]; then
+	die "Both KERNEL_DIR and WIFI_DIR must be set!"
 fi
 
 # shellcheck disable=SC2206
@@ -69,28 +67,25 @@ KCONFIG="$KERNEL_DIR/.config"
 # Prepare Kernel Tree
 rm -f "$KCONFIG"
 
-if [[ "$FULL_BUILD" == "true" ]]
-then
-    yes "" | make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" localmodconfig > /dev/null
-    make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" dir-pkg
+if [[ "$FULL_BUILD" == "true" ]]; then
+	yes "" | make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" localmodconfig >/dev/null
+	make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" dir-pkg
 else
-    make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" allyesconfig > /dev/null
-    make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" modules_prepare
+	make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" allyesconfig >/dev/null
+	make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" modules_prepare
 fi
 
 # Try to compile Wifi driver
-if make  -C "$WIFI_DIR" KBASE="$KERNEL_DIR" "${MAKE_OPTS[@]}"
-then
-    exit_code=0
+if make -C "$WIFI_DIR" KBASE="$KERNEL_DIR" "${MAKE_OPTS[@]}"; then
+	exit_code=0
 else
-    exit_code=1
+	exit_code=1
 fi
 
 # Cleanup
-if [[ "$CLEANUP" == "true" ]]
-then
-    make -C "$WIFI_DIR" KBASE="$KERNEL_DIR" "${MAKE_OPTS[@]}" clean
-    make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" distclean
+if [[ "$CLEANUP" == "true" ]]; then
+	make -C "$WIFI_DIR" KBASE="$KERNEL_DIR" "${MAKE_OPTS[@]}" clean
+	make -C "$KERNEL_DIR" "${MAKE_OPTS[@]}" distclean
 fi
 
 # Exit with correct code
